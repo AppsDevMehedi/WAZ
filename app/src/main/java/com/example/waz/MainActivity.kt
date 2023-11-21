95cda8eb615004aa44dc676e29fb42c701c0cbdf
Mehedi Hasan <hasanmahedi2578@gmail.com>
2023-11-21 13:11:23 -0800

    First Project

---
+++ app/src/main/java/com/example/waz/MainActivity.kt
@@ -0,0 +1,166 @@
+package com.example.waz
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.text.method.LinkMovementMethod
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val textView1: TextView = findViewById(R.id.textview1)
+            textView1.movementMethod = LinkMovementMethod.getInstance()
+
+        val textView2: TextView = findViewById(R.id.textview2)
+         textView2.movementMethod = LinkMovementMethod.getInstance()
+
+        val textView3: TextView = findViewById(R.id.textview3)
+        textView3.movementMethod = LinkMovementMethod.getInstance()
+
+        val textView4: TextView = findViewById(R.id.textview4)
+        textView4.movementMethod = LinkMovementMethod.getInstance()
+
+
+        val textView5: TextView = findViewById(R.id.textview5)
+        textView5.movementMethod = LinkMovementMethod.getInstance()
+
+        val textView6: TextView = findViewById(R.id.textview6)
+        textView6.movementMethod = LinkMovementMethod.getInstance()
+
+        val textView7: TextView = findViewById(R.id.textview7)
+        textView7.movementMethod = LinkMovementMethod.getInstance()
+
+        val textView8: TextView = findViewById(R.id.textview8)
+        textView8.movementMethod = LinkMovementMethod.getInstance()
+
+        val textView9: TextView = findViewById(R.id.textview9)
+        textView9.movementMethod = LinkMovementMethod.getInstance()
+
+        val textView10: TextView = findViewById(R.id.textview10)
+        textView10.movementMethod = LinkMovementMethod.getInstance()
+
+        val textView11: TextView = findViewById(R.id.textview11)
+        textView11.movementMethod = LinkMovementMethod.getInstance()
+
+        val textView12: TextView = findViewById(R.id.textview12)
+        textView12.movementMethod = LinkMovementMethod.getInstance()
+
+        val textView13: TextView = findViewById(R.id.textview13)
+        textView13.movementMethod = LinkMovementMethod.getInstance()
+
+        val textView14: TextView = findViewById(R.id.textview14)
+        textView14.movementMethod = LinkMovementMethod.getInstance()
+
+        val textView15: TextView = findViewById(R.id.textview15)
+        textView15.movementMethod = LinkMovementMethod.getInstance()
+
+        val textView16: TextView = findViewById(R.id.textview16)
+        textView16.movementMethod = LinkMovementMethod.getInstance()
+
+        val textView17: TextView = findViewById(R.id.textview17)
+        textView17.movementMethod = LinkMovementMethod.getInstance()
+
+        val textView18: TextView = findViewById(R.id.textview18)
+        textView18.movementMethod = LinkMovementMethod.getInstance()
+
+        val textView19: TextView = findViewById(R.id.textview19)
+        textView19.movementMethod = LinkMovementMethod.getInstance()
+
+        val textView20: TextView = findViewById(R.id.textview20)
+        textView20.movementMethod = LinkMovementMethod.getInstance()
+
+        val textView21: TextView = findViewById(R.id.textview21)
+        textView21.movementMethod = LinkMovementMethod.getInstance()
+
+        val textView22: TextView = findViewById(R.id.textview22)
+        textView22.movementMethod = LinkMovementMethod.getInstance()
+
+        val textView23: TextView = findViewById(R.id.textview23)
+        textView23.movementMethod = LinkMovementMethod.getInstance()
+
+        val textView24: TextView = findViewById(R.id.textview24)
+        textView24.movementMethod = LinkMovementMethod.getInstance()
+
+        val textView25: TextView = findViewById(R.id.textview25)
+        textView25.movementMethod = LinkMovementMethod.getInstance()
+
+        val textView26: TextView = findViewById(R.id.textview26)
+        textView26.movementMethod = LinkMovementMethod.getInstance()
+
+        val textView27: TextView = findViewById(R.id.textview27)
+        textView27.movementMethod = LinkMovementMethod.getInstance()
+
+        val textView28: TextView = findViewById(R.id.textview28)
+        textView28.movementMethod = LinkMovementMethod.getInstance()
+
+        val textView29: TextView = findViewById(R.id.textview29)
+        textView29.movementMethod = LinkMovementMethod.getInstance()
+
+        val textView30: TextView = findViewById(R.id.textview30)
+        textView30.movementMethod = LinkMovementMethod.getInstance()
+
+        val textView31: TextView = findViewById(R.id.textview31)
+        textView31.movementMethod = LinkMovementMethod.getInstance()
+
+        val textView32: TextView = findViewById(R.id.textview32)
+        textView32.movementMethod = LinkMovementMethod.getInstance()
+
+        val textView33: TextView = findViewById(R.id.textview33)
+        textView33.movementMethod = LinkMovementMethod.getInstance()
+
+        val textView34: TextView = findViewById(R.id.textview34)
+        textView34.movementMethod = LinkMovementMethod.getInstance()
+
+        val textView35: TextView = findViewById(R.id.textview35)
+        textView35.movementMethod = LinkMovementMethod.getInstance()
+
+        val textView36: TextView = findViewById(R.id.textview36)
+        textView36.movementMethod = LinkMovementMethod.getInstance()
+
+        val textView37: TextView = findViewById(R.id.textview37)
+        textView37.movementMethod = LinkMovementMethod.getInstance()
+
+        val textView38: TextView = findViewById(R.id.textview38)
+        textView38.movementMethod = LinkMovementMethod.getInstance()
+
+        val textView39: TextView = findViewById(R.id.textview39)
+        textView39.movementMethod = LinkMovementMethod.getInstance()
+
+        val textView40: TextView = findViewById(R.id.textview40)
+        textView40.movementMethod = LinkMovementMethod.getInstance()
+
+        val textView41: TextView = findViewById(R.id.textview41)
+        textView41.movementMethod = LinkMovementMethod.getInstance()
+
+        val textView42: TextView = findViewById(R.id.textview42)
+        textView42.movementMethod = LinkMovementMethod.getInstance()
+
+        val textView43: TextView = findViewById(R.id.textview43)
+        textView43.movementMethod = LinkMovementMethod.getInstance()
+
+        val textView44: TextView = findViewById(R.id.textview44)
+        textView44.movementMethod = LinkMovementMethod.getInstance()
+
+        val textView45: TextView = findViewById(R.id.textview45)
+        textView45.movementMethod = LinkMovementMethod.getInstance()
+
+        val textView46: TextView = findViewById(R.id.textview46)
+        textView46.movementMethod = LinkMovementMethod.getInstance()
+
+        val textView47: TextView = findViewById(R.id.textview47)
+        textView47.movementMethod = LinkMovementMethod.getInstance()
+
+        val textView48: TextView = findViewById(R.id.textview48)
+        textView48.movementMethod = LinkMovementMethod.getInstance()
+
+        val textView49: TextView = findViewById(R.id.textview49)
+        textView49.movementMethod = LinkMovementMethod.getInstance()
+
+        val textView50: TextView = findViewById(R.id.textview50)
+        textView50.movementMethod = LinkMovementMethod.getInstance()
+
+
+    }
+}
